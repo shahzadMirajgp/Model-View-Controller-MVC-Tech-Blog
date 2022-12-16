7fada1e0f69db3eccf98e312de752034441e2ff8
Chuca <Chuca78@users.noreply.github.com>
2022-12-16 17:27:48 -0500

    built models

--- models/index.js
+++ models/index.js
@@ -1,1 +1,37 @@
-// todo: build index model
+// todo: check index model
+
+// todo: make sure there are no more relationships
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = { User, Post, Comment };
