bbccbe7f333f127630d52fecaa094bcc94aac6e3
Chuca <Chuca78@users.noreply.github.com>
2022-12-16 08:29:56 -0500

    cleaned file structure and added seeds

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,1 @@
+// todo: build dashboard route js
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,1 +1,20 @@
 // todo: build user seeds js
+
+const { User } = require('../models');
+
+const userData = [
+    {
+        username: "Xandromus",
+        email: "Xandromus@gmail.com",
+        password: "p@ssword1"
+    },
+    {
+        username: "Lernantino",
+        email: "Lernantino@gmail.com",
+        password: "p@ssword2"
+    }
+]
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
