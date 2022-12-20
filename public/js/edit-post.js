9e3d176308a10277aa702381dde38ad210de5e4a
Chuca <Chuca78@users.noreply.github.com>
2022-12-20 13:17:34 -0500

    wrote README, updated public files

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,1 +1,49 @@
 // todo: build edit post js
+
+
+async function editFormHandler(event) {
+    event.preventDefault();
+
+  
+    const title = document.querySelector('input[name="post-title"]').value;
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+
+    const contents = document.querySelector('textarea[name="post-contents"]').value.trim();
+
+    if (title !== "" && contents !== "") {
+
+        const response = await fetch(`/api/posts/${id}`, {
+          method: 'PUT',
+          body: JSON.stringify({
+            title,
+            contents
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+
+        if (response.ok) {
+          document.location.replace('/dashboard/');
+        } else {
+          alert(response.statusText);
+        }
+    } else {
+
+        if (title === "" && contents === "") {
+          alert("Please enter a title and description, then submit")
+        } else if (title === "") {
+          alert("Please enter a title, then submit")
+        } else if (contents === "") {
+          alert("Please enter a description, then submit")
+        } else {
+          // Do nothing
+        }
+      
+    }
+  
+}
+  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+  
--- public/js/idle.js
+++ public/js/idle.js
@@ -1,1 +1,45 @@
 // todo: build idle js
+
+// Once the server start we will monitor for 
+
+
+
+function idleLogout() {
+    var timmer;
+    window.onload = resetTimer;
+    window.onmousemove = resetTimer;
+    window.onmousedown = resetTimer;  // catches touchscreen presses as well      
+    window.ontouchstart = resetTimer; // catches touchscreen swipes as well 
+    window.onclick = resetTimer;      // catches touchpad clicks as well
+    window.onkeydown = resetTimer;   
+    window.addEventListener('scroll', resetTimer, true); // 
+  
+    async function logout() {
+
+        alert("You have been idle for 5 minutes, logging you out.")
+
+        const response = await fetch('/api/users/logout', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' }
+          });
+      
+        
+          if (response.ok) {
+            //console.log("response of function log out good")
+            alert("Logging you out")
+            document.location.replace('/');
+          } else {
+            alert("You are already logged out")
+
+          }
+
+          
+   
+    }
+  
+    function resetTimer() {
+        clearTimeout(timmer);
+        timmer = setTimeout(logout, 300000);  // will automatically log you out in 5 minutes.
+    }
+  }
+  idleLogout();
